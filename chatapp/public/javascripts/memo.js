1891724a5533ee065d4ef8e1ef21eb6cc6802517
Chaty0shi <a17.k6bs@g.chuo-u.ac.jp>
2019-11-14 20:51:02 +0900

    committed in 2019/11/14 20:51:02

--- chatapp/public/javascripts/memo.js
+++ chatapp/public/javascripts/memo.js
@@ -10,8 +10,8 @@ function memo() {
     // 入力されたメッセージを取得
     const message = $('#message').val();
     // メモの内容を表示
-
-    $('#thread').prepend('<p>' + userName +':'+ message + '</p>');//ここに取得した変数を囲むことで投稿文表示
+    
+    $('#thread').prepend('<p>' + userName +'さんのメモ：'+ message + '</p>');//ここに取得した変数を囲むことで投稿文表示
 
 
     return false;
