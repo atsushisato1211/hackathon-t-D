05ef87aec2d9b1099b42a14f18db43bf6083a634
Chaty0shi <a17.k6bs@g.chuo-u.ac.jp>
2019-11-14 20:43:01 +0900

    committed in 2019/11/14 20:43:01

--- chatapp/public/javascripts/memo.js
+++ chatapp/public/javascripts/memo.js
@@ -1,13 +1,18 @@
 'use strict';
 
+
 // メモを画面上に表示する
 function memo() {
     // ユーザ名を取得
-    const userName = '';
+
+    //const userName = '';
+
     // 入力されたメッセージを取得
-    const message = '';
+    const message = $('#message').val();
     // メモの内容を表示
-    $('#thread').prepend('<p>' + '</p>');
+
+    $('#thread').prepend('<p>' + userName +':'+ message + '</p>');//ここに取得した変数を囲むことで投稿文表示
+
 
     return false;
 }
