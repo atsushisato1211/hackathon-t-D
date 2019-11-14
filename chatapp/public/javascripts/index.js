fbb0a514b80129ff1c979786685d2bb1781782e0
marinito628 <miel_fraise0628@yahoo.co.jp>
2019-11-14 18:19:34 +0900

    committed in 2019/11/14 18:19:34

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -2,13 +2,24 @@
 
 // チャットルームに入室する
 function enter() {
+
     // 入力されたユーザ名を取得する
     const userName = $('#user1').name();
+
     // ユーザ名が未入力でないかチェックする
+    //ユーザー名が空だったらアラート、空でなかったら入室
     if(userName === ''){
       const userAlert = "ユーザー名を入力してください。";
       alert("userAlert");
+      return false;
+
+    }else {
+      document.myform.action = "room.hbs";
     }
 
     $('form').submit();
+}
+
+router.get(,function(request,response)){
+  response.render('room',{userName1:userName})
 }
