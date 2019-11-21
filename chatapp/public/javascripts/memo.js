6698411d48331849ef07a8b6c55c1a7cc89f8a81
marinito628 <miel_fraise0628@yahoo.co.jp>
2019-11-21 20:19:28 +0900

    committed in 2019/11/21 20:19:28

--- chatapp/public/javascripts/memo.js
+++ chatapp/public/javascripts/memo.js
@@ -21,28 +21,24 @@ function memo() {
     var Sec = now.getSeconds();
     var times = "(" + Month + "月" + Dates+ "日" + Hour + "時" + Min + "分" + Sec + "秒" + ")";
     //色変更
-    
+    var element = document.getElementById("thread");
+    element.style.color = 'blue';
+
+
     // メモの内容を表示
-    if (message !=='') {
+    // 条件分岐
+      if( message.match(/\S/g)){ //空白、空行じゃないとき
+        //改行を表示（置き換える）
+        message.replace(/\r?\n/g, '<br>');
 
-      //色変更
-      　 //document.getElementById("thread").style.color = 'blue';
-        $('#thread').prepend('<p>' + times + userName +'さんのメモ：'+ message + '</p>');
-        //空文字
-        $('#message').val('');
+        //色変更
+        　 //document.getElementById("thread").style.color = 'blue';
+          $('#thread').prepend('<xmp>' + times + userName +'さんのメモ：'+ message +'\n'+'</xmp>');
+          //空文字
+          $('#message').val('');
 
 
-        //return false;
+      }
+
+
     }
-
-
-        //$('#thread').prepend('<p>' + userName +'さんのメモ：'+ message + formatted + '</p>');//ここに取得した変数を囲むことで投稿文表示
-//要素を空に
-        //$('#message').val('');
-
-
-    return false;
-    // 要素を空にする
-
-
-}
