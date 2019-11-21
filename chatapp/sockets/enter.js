d862bfa81035aaf448c29b4baa3f1ae387704ce0
KazumaKobayash <kazuma.kobayashi.3t@gmail.com>
2019-11-21 19:30:31 +0900

    committed in 2019/11/21 19:30:31

--- chatapp/sockets/enter.js
+++ chatapp/sockets/enter.js
@@ -2,7 +2,7 @@
 
 module.exports = function (socket) {
     // 入室メッセージをクライアントに送信する
-    socket.on('sendMessageEvent'', function (data) {
+    socket.on('sendMessageEvent', function (data) {
       if(!data){
         return;
       }
